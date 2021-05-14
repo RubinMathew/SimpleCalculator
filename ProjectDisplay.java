4c313d9b0c5e5afb17e11d52d50228be44dcff4a
Rubin Mathew <rubinullattu129@gmail.com>
2021-05-14 17:48:50 +0000

    Display service is committed to master branch

---
+++ ProjectDisplay.java
@@ -0,0 +1,6 @@
+public class ProjectDisplay{
+    public static void main(String[] args) {
+      
+        System.out.println("This is a Simple Calculator to perform some basic Mathematical Operations");
+    }
+}
